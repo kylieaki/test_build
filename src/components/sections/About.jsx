f562ed7e0e737a88fa8aef858224842f340e9a51
swath1mit <swath1@mit.edu>
2025-09-05 13:25:49 -0400

    Update About.jsx

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,15 +1,12 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
-  const frontendSkills = [
-    "React",
-    "Vue",
-    "TypeScript",
-    "TailwindCSS",
-    "Svelte",
-  ];
+  const buildProtocols = [
+    "3D Printing",
+    "Assembly",
+    "Electrode Fabrication",
 
-  const backendSkills = ["Node.js", "Python", "AWS", "MongoDB", "GraphQL"];
+  ];
 
   return (
     <section
@@ -20,20 +17,19 @@ export const About = () => {
         <div className="max-w-3xl mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             {" "}
-            About Me
+            Usage
           </h2>
 
           <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
             <p className="text-gray-300 mb-6">
-              Passionate developer with expertise in building scalable web
-              applications and creating innovative solutions.
+              Description.
             </p>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <h3 className="text-xl font-bold mb-4"> Protocols</h3>
                 <div className="flex flex-wrap gap-2">
-                  {frontendSkills.map((tech, key) => (
+                  {buildProtocols.map((tech, key) => (
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -46,62 +42,65 @@ export const About = () => {
                 </div>
               </div>
 
-              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Backend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {backendSkills.map((tech, key) => (
-                    <span
-                      key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
-                    "
-                    >
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-              </div>
+
             </div>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
-              <ul className="list-disc list-inside text-gray-300 space-y-2">
+              <h3 className="text-xl font-bold mb-4"> Author: </h3>
                 <li>
-                  <strong> B.S. in Computer Science </strong> - XYZ University
-                  (2016-2020)
+                  <strong> Department of Chemical Engineering </strong> MIT 2025
                 </li>
                 <li>
-                  Relevant Coursework: Data Structures, Web Development, Cloud
-                  Computing...
+                  Designed by: 
+                  {/* Social Media Icons */}
+                  <div className="flex gap-4 pt-2">
+                    {/* GitHub */}
+                    <a href="https://github.com/swath1pen" target="_blank" rel="noopener noreferrer">
+                      <img src="https://raw.githubusercontent.com/swath1pen/bioreactor/main/logos/github_logo.png" alt="GitHub" className="w-8 h-8 hover:scale-110 transition" />
+                    </a>
+                    {/* ORCID */}
+                    <a href="https://orcid.org/0000-0001-7566-8767" target="_blank" rel="noopener noreferrer">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/0/06/ORCID_iD.svg" alt="ORCID" className="w-8 h-8 hover:scale-110 transition" />
+                    </a>
+                    {/* BlueSky */}
+                    <a href="https://bsky.app/profile/swath1.bsky.social" target="_blank" rel="noopener noreferrer">
+                      <img src="https://raw.githubusercontent.com/swath1pen/bioreactor/main/logos/bluesky_logo.png" alt="GitHub" className="w-10 h-8 hover:scale-110 transition" />
+                    </a>
+                    {/* GScholar */}
+                    <a href="https://scholar.google.com/citations?user=QiISpWMAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">
+                      <img src="https://raw.githubusercontent.com/swath1pen/bioreactor/main/logos/google_scholar_logo.png" alt="ORCID" className="w-8 h-8 hover:scale-110 transition" />
+                    </a>
+                    {/* LinkedIn */}
+                    <a href="https://www.linkedin.com/in/swathi-penumutchu-135450b1" target="_blank" rel="noopener noreferrer">
+                      <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="w-8 h-8 hover:scale-110 transition" />
+                    </a>
+                    {/* Research Gate */}
+                    <a href="https://www.researchgate.net/profile/Swathi_Penumutchu" target="_blank" rel="noopener noreferrer">
+                      <img src="https://raw.githubusercontent.com/swath1pen/bioreactor/main/logos/research_gate_logo.png" alt="LinkedIn" className="w-8 h-8 hover:scale-110 transition" />
+                    </a>
+                  </div>
                 </li>
-              </ul>
             </div>
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+              <h3 className="text-xl font-bold mb-4"> Cite </h3>
               <div className="space-y-4 text-gray-300">
                 <div>
-                  <h4 className="font-semibold">
-                    {" "}
-                    Software Engineer at ABC Corp (2020 - Present){" "}
-                  </h4>
-                  <p>
-                    Developed and maintained microservices for cloud-based
-                    applications.
-                  </p>
-                </div>
+                  <h4 className="font-semibold">See Build GitHub Repo for citation details.</h4>
 
-                <div>
-                  <h4 className="font-semibold">
-                    {" "}
-                    Intern at DEF Startups (2019){" "}
-                  </h4>
-                  <p>
-                    Assisted in building front-end components and integration
-                    REST APIs
-                  </p>
                 </div>
+                  {/* Social Media Icons */}
+                  <div className="flex gap-4 pt-2">
+                    {/* GitHub */}
+                    <a href="https://github.com/swath1pen/bioreactor" target="_blank" rel="noopener noreferrer">
+                      <img src="https://raw.githubusercontent.com/swath1pen/bioreactor/main/logos/github_logo.png" alt="GitHub" className="w-8 h-8 hover:scale-110 transition" />
+                    </a>
+                    {/* BioEchemBuilds */}
+                    <a href="https://furstlab.github.io/BioEchemBuilds/" target="_blank" rel="noopener noreferrer">
+                      <img src="https://raw.githubusercontent.com/swath1pen/bioreactor/main/logos/bioechem_logo.png" alt="GitHub" className="w-50 h-12 hover:scale-110 transition" />
+                    </a>
+                  </div>
               </div>
             </div>
           </div>
